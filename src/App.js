28d3ed06317c63429cf7516c0ac32b22ac1a07b6
GreyGang <83516508+GreyGang@users.noreply.github.com>
2021-05-04 18:43:25 +0530

    Minor change in App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from "./Components/Navbar";
 import { GlobalStyles } from "./Assets/styles/GlobalStyles";
 import { MediaContextProvider, mediaStyles } from "./theme";
 import { leftItems, rightItems } from "./Assets/Data/NavbarData/index";
-import Button from "./Components/Button";
 function App() {
   return (
     <>
@@ -26,7 +25,6 @@ function App() {
             </Navbar>
           </Router>
           <GlobalStyles />
-          <Button>Demo Button</Button>
         </div>
       </MediaContextProvider>
     </>
