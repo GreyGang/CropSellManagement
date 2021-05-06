249a4d5fa3fa1235aee3dbdf878b3f7e45abdb33
Adarsh Mishra <adarshkaushik01@gmail.com>
2021-05-06 14:54:44 +0530

    changes in error page

--- src/Pages/Error/index.js
+++ src/Pages/Error/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Button from "./../../Components/Button";
+import styled from "styled-components";
 
+const H1 = styled.h1 `
+  text-align: center;
+`;
 export default function Error() {
   return (
     <div>
-      <h1>404</h1>
-      <h2>OOPS! PAGE NOT FOUND</h2>
+      <H1>404</H1>
+      <H1>OOPS! PAGE NOT FOUND</H1>
       <Button>Go To Homepage</Button>
     </div>
     
