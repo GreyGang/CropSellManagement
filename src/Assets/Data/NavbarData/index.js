788dc114bebaca8ab426874660323ada49a21bc0
mkp0 <shadymasum@gmail.com>
2021-05-06 15:15:48 +0530

    removed warning

--- src/Assets/Data/NavbarData/index.js
+++ src/Assets/Data/NavbarData/index.js
@@ -1,9 +1,15 @@
-import { home3, cart, blocked, rocket, userPlus } from "react-icons-kit/icomoon";
+import {
+  home3,
+  cart,
+  blocked,
+  rocket,
+  userPlus,
+} from "react-icons-kit/icomoon";
 
 const leftItems = [
   { content: "Home", to: "", id: 1, icon: home3 },
   { content: "Cart", to: "cart", id: 2, icon: cart },
-  { content: "Error", to: "error", id: 3, icon: cart },
+  { content: "Error", to: "error", id: 3, icon: blocked },
 ];
 const rightItems = [
   { content: "Login", to: "login", id: 1, icon: rocket },
