4acaada455d99551989900f8ece9e290d71e409c
Adarsh Mishra <adarshkaushik01@gmail.com>
2021-05-06 13:24:22 +0530

    added card and error page

--- src/Pages/Error/index.js
+++ src/Pages/Error/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Button from "./../../Components/Button";
 
 export default function Error() {
   return (
     <div>
-      <h1>Welcome to Error</h1>
+      <h1>404</h1>
+      <h2>OOPS! PAGE NOT FOUND</h2>
+      <Button>Go To Homepage</Button>
     </div>
+    
   );
 }
